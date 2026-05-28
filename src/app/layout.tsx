16c38c7d01054888a3aef8e03de35e41c41d2b71
Ysabella <ysabella@protonmail.com>
2026-05-28 15:18:32 +0800

    update default page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,8 +51,8 @@ export async function generateMetadata(): Promise<Metadata> {
   return {
     metadataBase: getBaseUrl(headerStore),
     title: {
-      template: '%s | Umami',
-      default: 'Umami',
+      template: '%s | ysabella',
+      default: 'site analytics — ysabella',
     },
   };
 }
